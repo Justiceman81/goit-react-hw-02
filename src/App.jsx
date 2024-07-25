2d3cdbaf97fa75feca1398f1dfd86e47313ba8a8
Justiceman81 <Justiceman814@gmail.com>
2024-07-25 21:03:15 +0300

    working options & feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
 
-const App = () => {};
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback =
+    Math.round((feedback.good / totalFeedback) * 100) || 0;
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+};
+
+const Notification = () => <p>No feedback given yet.</p>;
 
 export default App;
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,49 +1,10 @@
-import { useState, useEffect } from "react";
-const Options = () => {
-  const [first, setFirst] = useState(0);
-  const [second, setSecond] = useState(0);
-
-  useEffect(() => {
-    console.log("First updated: ", first);
-  }, [first]);
-
-  useEffect(() => {
-    console.log("Second updated: ", second);
-  }, [second]);
-
-  useEffect(() => {
-    console.log("First or second updated: ", first + second);
-  }, [first, second]);
-
-  return (
-    <>
-      <button onClick={() => setFirst(first + 1)}>First: {first}</button>
-      <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
-    </>
-  );
-};
+const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => (
+  <div>
+    <button onClick={() => updateFeedback("good")}>Good</button>
+    <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+    <button onClick={() => updateFeedback("bad")}>Bad</button>
+    {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+  </div>
+);
 
 export default Options;
-
-// const App = () => {
-//   const [clicks, setClicks] = useState(() => {
-//     const savedClicks = window.localStorage.getItem("saved-clicks");
-//     if (savedClicks !== null) {
-//       return savedClicks;
-//     }
-//     return 0;
-//   });
-
-//   useEffect(() => {
-//     window.localStorage.setItem("saved-clicks", clicks);
-//   }, [clicks]);
-
-//   return (
-//     <div>
-//       <button onClick={() => setClicks(clicks + 1)}>
-//         You clicked {clicks} times
-//       </button>
-//       <button onClick={() => setClicks(0)}>Reset</button>
-//     </div>
-//   );
-// };
